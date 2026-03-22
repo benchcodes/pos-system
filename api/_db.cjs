6e4c9f716fa2e1d76dafacde000a2726158bf434
Bench Culubong <culubongbenchmatthew@gmail.com>
2026-03-22 23:17:14 +0800

    new

---
+++ api/_db.cjs
@@ -0,0 +1,29 @@
+const dns = require('node:dns')
+const { MongoClient } = require('mongodb')
+
+// Use reliable public DNS to resolve MongoDB SRV records in serverless environments.
+dns.setServers(['1.1.1.1', '8.8.8.8'])
+
+const uri = process.env.ATLAS_URI
+const dbName = process.env.DB_NAME || 'pos_system'
+
+if (!uri) {
+  throw new Error('ATLAS_URI is missing in Vercel Environment Variables')
+}
+
+let clientPromise = global.__posMongoClientPromise
+
+if (!clientPromise) {
+  const client = new MongoClient(uri)
+  clientPromise = client.connect()
+  global.__posMongoClientPromise = clientPromise
+}
+
+async function getDb() {
+  const client = await clientPromise
+  return client.db(dbName)
+}
+
+module.exports = {
+  getDb,
+}
